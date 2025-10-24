b625677d4eb1b3d0a19c65b7d77eec58adf0302d
Jan Huls <j.huls@students.uu.nl>
2025-10-24 22:26:45 +0200

    bro ik ben hier kk klaar mee zlefs chat kan me niet helpen

--- MSOProgramLearningApp/MSOAvaloniaApp/ViewModels/MainWindowViewModel.cs
+++ MSOProgramLearningApp/MSOAvaloniaApp/ViewModels/MainWindowViewModel.cs
@@ -1,21 +1,51 @@
-﻿using MSOProgramLearningApp;
+﻿using System.Reactive;
+using System.Reactive.Concurrency;
+using MSOProgramLearningApp;
+using ReactiveUI;
+using System.Threading.Tasks;
 
 namespace MSOAvaloniaApp.ViewModels;
 
-public partial class MainWindowViewModel : ViewModelBase
+public partial class MainWindowViewModel : ReactiveObject
 {
-    private readonly Character _character;
-    
     public string Greeting { get; set; } = "bababa";
+    
+    private readonly Character _character;
+    public ReactiveCommand<Unit, Unit> OutputCommand { get; }
+    
+    private string _code = "type ur code here broski";
 
-
-    public MainWindowViewModel()
+    public string Code
     {
-        _character = new Character(Grid.TenSquareFalse());
+        get => _code;
+        set => this.RaiseAndSetIfChanged(ref _code, value);
+    }
+    private string _output = "Output";
+    public string Output
+    {
+        get => _output;
+        private set => this.RaiseAndSetIfChanged(ref _output, value);
     }
 
     public MainWindowViewModel(Character character)
     {
         _character = character;
+        OutputCommand = ReactiveCommand.CreateFromTask(async () =>
+        {
+            // parse code off the UI thread
+            var cmds = await Task.Run(() => new StringParser(Code).Parse());
+
+            // update UI on main thread
+            var c = _character;
+            foreach (var cmd in cmds)
+                cmd.Execute(c);
+
+            Output = string.Join(", ", c.Moves) + ".";
+            Output += $"\nEnd state {c}";
+        }, outputScheduler: RxApp.MainThreadScheduler);
+    }
+
+    public MainWindowViewModel() : this(new Character(Grid.TenSquareFalse()))
+    {
     }
 }
--- MSOProgramLearningApp/MSOAvaloniaApp/Views/MainWindow.axaml.cs
+++ MSOProgramLearningApp/MSOAvaloniaApp/Views/MainWindow.axaml.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using MSOAvaloniaApp.ViewModels;
+using MSOProgramLearningApp;
 
 namespace MSOAvaloniaApp.Views;
 
@@ -10,6 +11,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new MainWindowViewModel();
     }
     private void LoadHandler(object? sender, RoutedEventArgs e)
     {
