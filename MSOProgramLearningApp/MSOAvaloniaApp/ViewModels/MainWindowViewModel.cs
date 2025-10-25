7daa5601d5212583c79526af91d27714545b35b4
Jan Huls <j.huls@students.uu.nl>
2025-10-25 13:03:04 +0200

    hij werukt

--- MSOProgramLearningApp/MSOAvaloniaApp/ViewModels/MainWindowViewModel.cs
+++ MSOProgramLearningApp/MSOAvaloniaApp/ViewModels/MainWindowViewModel.cs
@@ -1,50 +1,41 @@
-﻿using System.Reactive;
-using System.Reactive.Concurrency;
-using MSOProgramLearningApp;
-using ReactiveUI;
-using System.Threading.Tasks;
+﻿using MSOProgramLearningApp;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace MSOAvaloniaApp.ViewModels;
 
-public partial class MainWindowViewModel : ReactiveObject
+public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; set; } = "bababa";
+    private readonly Character _character;   
     
-    private readonly Character _character;
-    public ReactiveCommand<Unit, Unit> OutputCommand { get; }
+    [ObservableProperty]
+    private string greeting = "bababa";
     
-    private string _code = "type ur code here broski";
+    [ObservableProperty]
+    private string code = "type ur code here broski";
 
-    public string Code
-    {
-        get => _code;
-        set => this.RaiseAndSetIfChanged(ref _code, value);
-    }
-    private string _output = "Output";
-    public string Output
-    {
-        get => _output;
-        private set => this.RaiseAndSetIfChanged(ref _output, value);
-    }
-
+    [ObservableProperty] 
+    private string output = "Output";
+    
+    public IRelayCommand OutputCommand { get; }
+    
     public MainWindowViewModel(Character character)
     {
         _character = character;
-        OutputCommand = ReactiveCommand.CreateFromTask(async () =>
-        {
-            // parse code off the UI thread
-            var cmds = await Task.Run(() => new StringParser(Code).Parse());
-
-            // update UI on main thread
-            var c = _character;
-            foreach (var cmd in cmds)
-                cmd.Execute(c);
-
-            Output = string.Join(", ", c.Moves) + ".";
-            Output += $"\nEnd state {c}";
-        }, outputScheduler: RxApp.MainThreadScheduler);
+        OutputCommand = new RelayCommand(GenerateOutput);
     }
 
+    private void GenerateOutput()
+    {
+        var cmds = new StringParser(Code).Parse();
+        
+        var c = _character;
+        foreach (var cmd in cmds)
+            cmd.Execute(c);
+
+        Output = string.Join(", ", c.Moves) + ".";
+        Output += $"\nEnd state {c}";
+    }
     public MainWindowViewModel() : this(new Character(Grid.TenSquareFalse()))
     {
     }
