c04aa42339e13b930f16785b3c05c799ab366e2e
bionic85 <144353436+bionic85@users.noreply.github.com>
2025-10-27 15:22:18 +0100

    pressing RUN will now generate a 10x10 grid using ImageSharp

--- MSOProgramLearningApp/MSOAvaloniaApp/ViewModels/MainWindowViewModel.cs
+++ MSOProgramLearningApp/MSOAvaloniaApp/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
 using MSOProgramLearningApp;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -7,23 +10,27 @@ namespace MSOAvaloniaApp.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    private readonly Character _character;   
+    private readonly Character _character;
+    private OutputDrawer _outputDrawer;
     
     [ObservableProperty]
     private string greeting = "bababa";
     
-    [ObservableProperty]
-    private string code = "type ur code here broski";
+    [ObservableProperty] // example code of FindExit1
+    private string code = "Repeat 3 \n    RepeatUntil WallAhead \n        Move 1 \n    Turn right \nRepeat 2 \n    RepeatUntil GridEdge \n        Move 1 \n    Turn right\n";
 
     [ObservableProperty] 
     private string output = "Output";
     
     public IRelayCommand MetricsCommand { get; } 
     public IRelayCommand OutputCommand { get; }
-    
+
+    [ObservableProperty] private IImage? outputImageBinding;
+
     public MainWindowViewModel(Character character)
     {
         _character = character;
+        _outputDrawer = new OutputDrawer();
         OutputCommand = new RelayCommand(GenerateOutput);
         MetricsCommand = new RelayCommand(GenerateMetrics);
     }
@@ -40,9 +47,13 @@ public partial class MainWindowViewModel : ViewModelBase
         var metrics = new MetricsCalculator(new BasicMetricsStrategy());
         Output = metrics.Calculate(cmds);
     }
+
     private void GenerateOutput()
     {
         var cmds = getCommands(Code);
+        MemoryStream memoryStream = new MemoryStream();
+        _outputDrawer.GenerateBitmap(cmds, _character,memoryStream);
+        OutputImageBinding = new Bitmap(memoryStream);
         
         var c = _character;
         foreach (var cmd in cmds)
@@ -50,6 +61,7 @@ public partial class MainWindowViewModel : ViewModelBase
 
         Output = string.Join(", ", c.Moves) + ".";
         Output += $"\nEnd state {c}";
+        
     }
     public MainWindowViewModel() : this(new Character(Grid.TenSquareFalse()))
     {
---
+++ MSOProgramLearningApp/MSOProgramLearningApp/OutputDrawer.cs
@@ -0,0 +1,75 @@
+﻿using System.Diagnostics;
+using Avalonia.Controls.Shapes;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Drawing;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Processing.Processors;
+using Point = Avalonia.Point;
+
+namespace MSOProgramLearningApp;
+
+// can create an image of the output given a set of instructions
+public class OutputDrawer(int size = 1000) // should be a square so width and height are the same
+{
+    private readonly int _size = size;
+
+    public void GenerateBitmap(List<ICommand> commands, Character character, MemoryStream outputMemStream)
+    {
+        Image<Rgba32> image = new Image<Rgba32>(_size, _size);
+        image.Mutate(ctx =>
+        {
+            // draw the grid
+            ctx.DrawImage(GenerateGridImage(character.Grid), 1);
+            
+        });
+        
+        // save the image to memory
+        image.SaveAsBmp(outputMemStream);
+        outputMemStream.Position = 0;
+    }
+
+    // generates an image of the grid
+    public Image<Rgba32> GenerateGridImage(Grid grid)
+    {
+        Image<Rgba32> image = new Image<Rgba32>(_size, _size);
+        Color color = Color.Black;
+        int spaceBetweenLines = _size / grid.GetWidth();
+        float thickness = spaceBetweenLines / 5f;
+        
+        Debug.Assert(grid.GetHeight() == grid.GetWidth()); // for now, we only support square grids
+
+        
+        image.Mutate(ctx =>
+        {
+            // background and border
+            ctx.Fill(Color.White);
+            ctx.Draw(color, thickness, new RectangularPolygon(0, 0, _size, _size));
+            
+            // grid
+            for (int i = 1; i < grid.GetWidth(); i++)
+            {
+                PointF[] verticalPoints = new PointF[2];
+                verticalPoints[0] = new PointF(i * spaceBetweenLines, 0);
+                verticalPoints[1] = new PointF(i * spaceBetweenLines, _size);
+                
+                PointF[] horizontalPoints = new PointF[2];
+                horizontalPoints[0] = new PointF(0, i * spaceBetweenLines);
+                horizontalPoints[1] = new PointF(_size, i * spaceBetweenLines);
+                
+                ctx.DrawLine(color, thickness, verticalPoints);
+                ctx.DrawLine(color, thickness, horizontalPoints);
+            }
+        });
+
+        image.SaveAsBmp("grid.bmp"); //test doet output in root folder waar de .exe staat
+        return image;
+    }
+
+    private Image<Rgba32> DrawCommand(ICommand command)
+    {
+        throw new NotImplementedException();
+    }
+}
